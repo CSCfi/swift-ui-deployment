008bb07a49d24b94a44ac45a02a91fdf40866892
sampsapenna <sampsa.penna@csc.fi>
2019-12-05 14:20:51 +0200

    Fix incorrect table name in initialization

--- integration/init-project-db.sh
+++ integration/init-project-db.sh
@@ -13,7 +13,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
 EOSQL
 
 psql -v ON_ERROR_STOP=1 --username "sharing" --dbname "swiftsharing" <<-EOSQL
-    CREATE TABLE IF NOT EXISTS Sharing(
+    CREATE TABLE IF NOT EXISTS Shares(
         container TEXT,
         container_owner TEXT,
         recipient TEXT,
